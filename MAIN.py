85df54b8819a09d347132c6082875018173331d6
NeonG4 <101948482+NeonG4@users.noreply.github.com>
2023-03-04 17:10:28 -0800

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,7 +1,12 @@
-import os 
-words = []
+import os, random
+file = open("words.txt", "r")
+word = readline(random.randint(0, 5756))
 print("Hello world")
 os.system("clear")
 while True:
   guess = input("What is your guess?")
-  
+  #testcase
+  if guess == word:
+    print("Nice job!")
+  else:
+    print("Try again...")
