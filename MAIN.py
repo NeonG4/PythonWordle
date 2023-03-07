451eba3590c56723f388b7e414035df198ea22b6
Igbalabanov <101612094+Igbalabanov@users.noreply.github.com>
2023-03-07 12:06:56 -0800

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,12 +1,15 @@
 import os, random
-file = open("words.txt", "r")
-word = readline(random.randint(0, 5756))
-print("Hello world")
-os.system("clear")
-while True:
-  guess = input("What is your guess?")
-  #testcase
-  if guess == word:
-    print("Nice job!")
-  else:
-    print("Try again...")
+def Main():
+  file = open("words.txt", "r")
+  word = readline(random.randint(0, 5756))
+  print("Hello world")
+  os.system("clear")
+  while True:
+    guess = input("What is your guess?")
+    #testcase
+    if guess == word:
+      print("Nice job!")
+    else:
+      print("Try again...")
+if "__name__" == __main__:
+  Main()
